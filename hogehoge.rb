bd819a117795fc71593963bcb5978a7e3d6eaf76
Seita Uchimura <c201552k@gmail.com>
2017-11-27 16:13:04 +0900

    initial commit

--- hogehoge.rb
+++ hogehoge.rb
@@ -1,6 +1,6 @@
 # o = Dir::entries(".")
 # i = Dir::glob("*/")
-# s = nil
+
 CurrentDir = Dir::entries(".")
 
 p CurrentDir
@@ -17,26 +17,6 @@ FileOnly.each do |hoge|
   p hoge
 end
 
-# # i.each do |hoge|
-# #   s << hoge.delete("/")
-# # end
 
-# # p s
-
-# # i.each do |hoge|
-# #   p hoge.to_s
-# # end
-
-# # o.each do |hoge|
-# #   p hoge.to_s
-# # end
-
-# o.each do |del|
-#   o.delete(del) if i.include?(del)
-# end
-
-# puts o
-# puts "\n"
-# puts i
 
 
