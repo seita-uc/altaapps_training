7ab141ef6cb5e778779b42f36bbe7205bb7d4959
Seita Uchimura <c201552k@gmail.com>
2017-11-27 16:44:44 +0900

    $B%3%a%s%H%"%&%H$N>C5n(B

--- hogehoge.rb
+++ hogehoge.rb
@@ -1,6 +1,4 @@
-# o = Dir::entries(".")
-# i = Dir::glob("*/")
-# s = nil
+
 CurrentDir = Dir::entries(".")
 
 p CurrentDir
@@ -16,27 +14,5 @@ p FileOnly
 FileOnly.each do |hoge|
   p hoge
 end
-
-# # i.each do |hoge|
-# #   s << hoge.delete("/")
-# # end
-
-# # p s
-
-# # i.each do |hoge|
-# #   p hoge.to_s
-# # end
-
-# # o.each do |hoge|
-# #   p hoge.to_s
-# # end
-
-# o.each do |del|
-#   o.delete(del) if i.include?(del)
-# end
-
-# puts o
-# puts "\n"
-# puts i
 
 
