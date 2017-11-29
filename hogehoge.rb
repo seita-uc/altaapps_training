cf4064fadc71976ca4fe425e64a891d5ed4d444d
Seita Uchimura <c201552k@gmail.com>
2017-11-29 15:35:01 +0900

    created methods for each function

--- hogehoge.rb
+++ hogehoge.rb
@@ -1,42 +1,51 @@
-fileEntries = Dir::entries(".")
-puts  fileEntries 
-puts
-
-onlyFiles = Array.new
-
-#ファイルエントリからディレクトリを削除
-fileEntries.each do |x|
-	onlyFiles = fileEntries.delete_if{|x| File::ftype(x) == "directory"}
+def getAllFileNamesInCurrentDir
+	allFileNames = Dir::entries(".")
+	puts  allFileNames 
+	return allFileNames
 end
 
-puts  onlyFiles
-puts
+def extractOnlyFiles(allFileNames)
+	onlyFiles = Array.new
 
-
-f_txt = Array.new 
-f_html = Array.new 
-f_css = Array.new 
-f_md = Array.new 
-f_etc = Array.new 
-
-onlyFiles.each do |x|
-	if    File.extname(x) == ".txt"
-		f_txt.push(x) 
-	elsif File.extname(x) == ".html"		
-		f_html.push(x) 
-	elsif File.extname(x) == ".css"		
-		f_css.push(x)		
-	elsif File.extname(x) == ".md"				
-		f_md.push(x)		
-	else 
-		f_etc.push(x)	
+	allFileNames.each do |x|
+		onlyFiles = allFileNames.delete_if{|x| File::ftype(x) == "directory"}
 	end
+
+	puts  onlyFiles
+	return onlyFiles	
 end
 
-puts "txt file: #{f_txt}"
-puts "html file: #{f_html}"
-puts "css file: #{f_css}"
-puts "md file: #{f_md}"
-puts "etc: #{f_etc}"
+def assortFileTypes(onlyFiles)
+	f_txt = Array.new 
+	f_html = Array.new 
+	f_css = Array.new 
+	f_md = Array.new 
+	f_etc = Array.new 
 
+	onlyFiles.each do |x|
+		if    File.extname(x) == ".txt"
+			f_txt.push(x) 
+		elsif File.extname(x) == ".html"		
+			f_html.push(x) 
+		elsif File.extname(x) == ".css"		
+			f_css.push(x)		
+		elsif File.extname(x) == ".md"				
+			f_md.push(x)		
+		else 
+			f_etc.push(x)	
+		end
+	end
+	
+	puts "txt file: #{f_txt}"
+	puts "html file: #{f_html}"
+	puts "css file: #{f_css}"
+	puts "md file: #{f_md}"
+	puts "etc: #{f_etc}"
+end
+
+fileEntries = Array.new
+fileEntries  = getAllFileNamesInCurrentDir
+onlyFiles = Array.new
+onlyFiles = extractOnlyFiles(fileEntries)
+assortFileTypes(onlyFiles)
 	
