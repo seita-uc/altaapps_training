966be08f5d4abc06d95ecdf093a2c11b00452d81
Seita Uchimura <c201552k@gmail.com>
2017-12-02 15:17:41 +0900

    finished until num7

---
+++ go_training/num6.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+import "strings"
+
+func sentenceIntoWords(sentence string) []string {
+	split_str := []rune(sentence)
+	var word []string
+	var splited_words []string
+	for i, v := range split_str {
+		if string(v) != " " && string(v) != "," && string(v) != "." {
+			word = append(word, string(v))
+			if i == len(split_str)-1 {
+				splited_words = append(splited_words, strings.Join(word, ""))
+			}
+		} else {
+			if len(word) == 0 {
+				continue
+			}
+			splited_words = append(splited_words, strings.Join(word, ""))
+			word = nil
+		}
+	}
+	return splited_words
+}
+
+func sentenceIntoLetters(sentence string) []string {
+	split_str := []rune(sentence)
+	var letters []string
+	for _, v := range split_str {
+		if string(v) != " " && string(v) != "," && string(v) != "." {
+			letters = append(letters, string(v))
+		}
+	}
+	return letters
+}
+
+func createNgram(w_ele []string, l_ele []string) ([]string, []string) {
+
+	//単語bi-gram
+	var word_result []string
+	for i := 0; i < len(w_ele); i++ {
+		if i+1 == len(w_ele) {
+			break
+		}
+		word_result = append(word_result, strings.Join(w_ele[i:i+2], "-"))
+	}
+
+	//文字bi-gram
+	var letter_result []string
+	for i := 0; i < len(l_ele); i++ {
+		if i+1 == len(l_ele) {
+			break
+		}
+		letter_result = append(letter_result, strings.Join(l_ele[i:i+2], "-"))
+	}
+
+	return word_result, letter_result
+}
+
+func main() {
+
+	fmt.Println(createNgram(sentenceIntoWords("I am an NLPer"), sentenceIntoLetters("I am an NLPer")))
+}
---
+++ go_training/num7.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+import "strings"
+
+func create_ngram(sentence string) []string {
+
+	letters := strings.Split(sentence, "")
+
+	//文字bi-gram
+	var letterResult []string
+	for i := 0; i < len(letters); i++ {
+		if i+1 == len(letters) {
+			break
+		}
+		letterResult = append(letterResult, strings.Join(letters[i:i+2], ""))
+	}
+	return remove_duplication(letterResult)
+}
+
+func remove(search string, set []string) []string {
+	result := []string{}
+	for _, v := range set {
+		if v != search {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func include(s string, set []string) bool {
+	for _, v := range set {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func union(set1 []string, set2 []string) []string {
+	new_str := []string{}
+	for _, v := range set1 {
+		if !include(v, set2) {
+			new_str = append(new_str, v)
+		}
+	}
+	new_str = append(new_str, set2...)
+	return new_str
+}
+
+func remove_duplication(set []string) []string {
+	m := make(map[string]bool)
+	new_str := []string{}
+	for _, v := range set {
+		if !m[v] {
+			m[v] = true
+			new_str = append(new_str, v)
+		}
+	}
+	return new_str
+}
+
+func intersection(set1 []string, set2 []string) []string {
+	num := 0
+	var intersect_str []string
+	for _, v := range set1 {
+		for i, s := range set2 {
+			if v == s {
+				num++
+			}
+			if i == len(set2) {
+				break
+			}
+		}
+		if num == 1 {
+			intersect_str = append(intersect_str, v)
+		}
+		num = 0
+	}
+	return intersect_str
+}
+
+func difference(set1 []string, set2 []string) []string {
+	var new_str []string
+	for _, v := range set1 {
+		if !include(v, set2) {
+			new_str = append(new_str, v)
+		}
+	}
+	return new_str
+}
+
+func main() {
+	//文字bi-gramを求める
+	//重複したbi-gramは取り除く
+	setX := (create_ngram("paraparaparadise"))
+	setY := (create_ngram("paragraph"))
+
+	//XとYの和集合
+	fmt.Println(union(setX, setY))
+
+	//XとYの積集合
+	fmt.Println(intersection(setX, setY))
+
+	//XとYの差集合
+	fmt.Println(difference(setX, setY))
+
+	//"se"という文字列がXとYに含まれるかどうか
+	fmt.Println(include("se", setX))
+	fmt.Println(include("se", setY))
+}
